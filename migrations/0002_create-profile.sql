caba8ea6e74ddf7b7efe8d7c440074bb33e6d689
kbventures <kmb_x@hotmail.com>
2025-03-03 13:32:03 -0500

    Migration update

--- migrations/0002_create-profile.sql
+++ migrations/0002_create-profile.sql
@@ -75,6 +75,24 @@ CREATE TABLE IF NOT EXISTS passwords (
 	FOREIGN KEY (id) REFERENCES profile(id)
 );
 
+-- Migration number: 0005 	 2025-01-30T00:58:45.123Z
+
+DROP TABLE IF EXISTS access;
+
+-- Store authentication and authorization data separately from profiles
+CREATE TABLE IF NOT EXISTS access (
+	-- The UUID of the profile this access belongs to
+	id TEXT NOT NULL UNIQUE COLLATE BINARY,
+	-- Schema version to use
+	schemaVersion INTEGER NOT NULL DEFAULT 1,
+	-- The person's access level in the system
+	-- This determines what actions they can perform
+	access TEXT NOT NULL CHECK(access IN ('admin', 'organizer', 'volunteer')),
+	-- The date this access was added to the database
+	FOREIGN KEY (id) REFERENCES profile(id)
+);
+
+
 DROP TABLE IF EXISTS profile_links;
 
 CREATE TABLE IF NOT EXISTS profile_links (
