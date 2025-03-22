1395d98222280dd9c4faa3ce8ddc27d93ddab154
Abdul Shabbir <abdulqshabbir@gmail.com>
2025-03-22 13:45:22 +0800

    fix: :bug: add generic unauth message for auth middleware token

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -6,19 +6,20 @@ import { StatusCodes } from '../utils/responses.ts';
 export const authMiddleware = async (context: Context, next: Next) => {
 	// Get token from cookie
 	const sessionToken: string | undefined = getCookie(context, 'auth_token');
+	const genericUnauthorizedMessage = 'Invalid or missing token';
 
 	if (!sessionToken) {
-		return context.json({ message: 'Invalid or missing token' }, StatusCodes.UNAUTHORIZED);
+		return context.json({ message: genericUnauthorizedMessage }, StatusCodes.UNAUTHORIZED);
 	}
 	const sessionData: SessionData | undefined = await context.env.SESSION_TOKENS.get(sessionToken, 'json');
 
 	if (!sessionData) {
-		return context.json({ message: 'Invalid session' }, StatusCodes.UNAUTHORIZED);
+		return context.json({ message: genericUnauthorizedMessage }, StatusCodes.UNAUTHORIZED);
 	}
 
 	if (new Date(sessionData.expiry) < new Date()) {
 		await context.env.SESSION_TOKENS.delete(sessionToken);
-		return context.json({ message: 'Session expired' }, StatusCodes.UNAUTHORIZED);
+		return context.json({ message: genericUnauthorizedMessage }, StatusCodes.UNAUTHORIZED);
 	}
 
 	context.set('session', sessionData);
