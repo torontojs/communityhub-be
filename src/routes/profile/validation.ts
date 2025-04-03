9b7f3c4df302d98403781216a2c651e1b5734146
Brandon Cha <bcha92@hotmail.com>
2025-04-02 20:50:16 -0400

    restructure refine on birthday

--- src/routes/profile/validation.ts
+++ src/routes/profile/validation.ts
@@ -16,9 +16,9 @@ export const ProfileSchema = BaseDbEntitySchema.merge(z.object({
 		.optional()
 		.describe('Pronouns the user prescribes to'),
 	birthday: z
-		.optional(
-			z.string().refine((mmdd) => /([0][1-9]|[1][0-2])-([0][1-9]|[1-2][0-9]|[3][0-1])/u.exec(mmdd), { message: 'Birthday must be valid MM/DD format or nullish value' })
-		)
+		.string()
+		.refine((mmdd) => /([0][1-9]|[1][0-2])-([0][1-9]|[1-2][0-9]|[3][0-1])/u.exec(mmdd), { message: 'Birthday must be valid MM-DD format' })
+		.optional()
 		.describe('Birthday of user'),
 	isBasedOnGTA: z
 		.number()
