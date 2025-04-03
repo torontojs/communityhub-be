c7c95f5c22ae9a067b47ee432ada04f3ae02bfaf
Brandon Cha <bcha92@hotmail.com>
2025-04-02 20:38:09 -0400

    With a dash, not a slash

--- src/routes/profile/validation.ts
+++ src/routes/profile/validation.ts
@@ -17,7 +17,7 @@ export const ProfileSchema = BaseDbEntitySchema.merge(z.object({
 		.describe('Pronouns the user prescribes to'),
 	birthday: z
 		.optional(
-			z.string().refine((mmdd) => /([0][1-9]|[1][0-2])\/([0][1-9]|[1-2][0-9]|[3][0-1])/u.exec(mmdd), { message: 'Birthday must be valid MM/DD format or nullish value' })
+			z.string().refine((mmdd) => /([0][1-9]|[1][0-2])-([0][1-9]|[1-2][0-9]|[3][0-1])/u.exec(mmdd), { message: 'Birthday must be valid MM/DD format or nullish value' })
 		)
 		.describe('Birthday of user'),
 	isBasedOnGTA: z
