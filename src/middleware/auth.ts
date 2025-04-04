fec715ec1f18f14b91ceeff54d73b020c799a0fb
Abdul Shabbir <abdulqshabbir@gmail.com>
2025-04-04 22:05:36 +0800

    refactor: :recycle: remove redundant checks in auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,37 +1,16 @@
 import type { Context, Next } from 'hono';
 import {
-	deleteSession,
-	extendExistingSession,
-	getSession,
-	isSesionExpired,
-	shouldSessionExtend
+	getSession
 } from 'src/utils/auth.ts';
 import { StatusCodes } from '../utils/responses.ts';
 
 export const authMiddleware = async (context: Context, next: Next) => {
-	const { session, sessionToken } = await getSession(context);
+	const { session } = await getSession(context);
 
 	const invalidSessionResponse = { message: 'Invalid session' };
 
 	if (!session) {
 		return context.json(invalidSessionResponse, StatusCodes.UNAUTHORIZED);
-	}
-
-	const isExpired = isSesionExpired(session.expiry);
-
-	if (isExpired) {
-		await deleteSession({ context, sessionToken });
-		return context.json(invalidSessionResponse, StatusCodes.UNAUTHORIZED);
-	}
-
-	const shouldExtendTokenExpiry = shouldSessionExtend(session.expiry);
-
-	if (shouldExtendTokenExpiry) {
-		await extendExistingSession({
-			sessionToken,
-			session,
-			context
-		});
 	}
 
 	context.set('session', session);
