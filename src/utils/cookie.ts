6af2065dd16d645d5d2bfb2b033322a08e1ece6f
kbventures <kmb_x@hotmail.com>
2025-04-05 20:19:07 -0400

    setCookiePreset

---
+++ src/utils/cookie.ts
@@ -0,0 +1,22 @@
+import { type Context } from 'hono';
+import { setCookie } from 'hono/cookie';
+import { type CookieOptions } from 'hono/utils/cookie.js';
+
+const DEFAULT_COOKIE_OPTIONS: CookieOptions = {
+			path: '/',
+			secure: true,
+			httpOnly: true,
+			sameSite: 'Strict',
+}
+
+export function presetSetCookie(
+	context: Context<EnvironmentBindings>,
+	name: string,
+	value: string,
+	expires: Date,
+): void {
+	setCookie(context, name, value, {
+		...DEFAULT_COOKIE_OPTIONS,
+		expires,
+	})
+}
