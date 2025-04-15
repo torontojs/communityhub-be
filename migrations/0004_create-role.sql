c3011b34886483675f15b4d99f9084d0131e05ba
madcampos <me@madcampos.dev>
2025-04-15 01:30:29 -0400

    fix: re-apply changes overriten by event log branch

--- migrations/0004_create-role.sql
+++ migrations/0004_create-role.sql
@@ -1,6 +1,5 @@
 -- Migration number: 0004 	 2025-01-30T00:58:39.397Z
 
-DROP INDEX IF EXISTS idx_role_profile_and_team ON role;
 DROP TABLE IF EXISTS role;
 
 -- The role a person may have on a team
@@ -14,9 +13,9 @@ CREATE TABLE IF NOT EXISTS role (
 	-- The role description
 	description TEXT,
 	-- The UUID of the team this role belongs to
-	team_id TEXT NOT NULL COLLATE BINARY,
+	teamId TEXT NOT NULL COLLATE BINARY,
 	-- The UUID of the profile this role is assigned to
-	profile_id TEXT NOT NULL COLLATE BINARY,
+	profileId TEXT NOT NULL COLLATE BINARY,
 	-- The date this role was assigned, saved as an ISO timestamp
 	happenedAt DATETIME NOT NULL,
 	-- The date this role was added to the database, saved as an ISO timestamp
@@ -25,8 +24,8 @@ CREATE TABLE IF NOT EXISTS role (
 	deletedAt DATETIME DEFAULT NULL,
 
 	PRIMARY KEY (id),
-	FOREIGN KEY (team_id) REFERENCES team(id),
-	FOREIGN KEY (profile_id) REFERENCES profile(id)
+	FOREIGN KEY (teamId) REFERENCES team(id),
+	FOREIGN KEY (profileId) REFERENCES profile(id)
 );
 
 CREATE INDEX idx_role_profile_and_team ON role (profileId, teamId);
