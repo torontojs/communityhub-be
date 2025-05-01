3f415de15841cb6ad31c86a392f6f28bf5d90cca
madcampos <madcampos@outlook.com>
2025-05-01 17:23:41 -0400

    fix: resolve issue with server port

--- curl-scripts/sign-up.sh
+++ curl-scripts/sign-up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Directly use the credentials for authentication
-curl -X POST http://localhost:8787/auth/sign-up \
+curl -X POST http://localhost:4242/auth/sign-up \
   -H "Content-Type: application/json" \
   -d '{
     "email": "profile4@example.com",
