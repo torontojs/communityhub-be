4cd54912ae7c60a498ef30d66d1e216260f5ea9a
kbventures <kmb_x@hotmail.com>
2025-05-01 14:31:58 -0400

    Vitest setup

--- src/routes/auth/index.test.ts
+++ src/routes/auth/index.test.ts
@@ -4,10 +4,10 @@ import app from '../../index.ts';
 
 describe('Sign-in route', () => {
 	it('Success', async () => {
-		await env.Database.exec(`
-      INSERT INTO users (email, password_hash)
-      VALUES ('test@example.com', 'hashed_password123')
-    `);
+		// 	Await env.Database.exec(`
+		//   INSERT INTO users (email, password_hash)
+		//   VALUES ('test@example.com', 'hashed_password123')
+		// `);
 
 		const response = await app.request('/auth/sign-in', {
 			method: 'POST',
@@ -18,7 +18,28 @@ describe('Sign-in route', () => {
 			})
 		}, env);
 
-		// Verify
 		expect(response.status).toBe(200);
 	});
 });
+// Import { describe, expect, test } from 'vitest';
+// Import app from '../../index.ts';
+// Import type { StatusResponse } from '../../utils/responses.ts';
+// Import { MockEnvBindings } from '../../utils/testing.ts';
+
+// Const MOCK_ENV = new MockEnvBindings();
+
+// Describe('Sign-in route', () => {
+// 	Test('Success', async () => {
+// 		Const response = await app.request('/auth/sign-in', {
+// 			Method: 'POST',
+// 			Headers: new Headers({ 'Content-Type': 'application/json' }),
+// 			Body: JSON.stringify({
+// 				Email: 'test@example.com',
+// 				Password: 'password123'
+// 			})
+// 		}, MOCK_ENV);
+// 		Const json: StatusResponse = await response.json();
+
+// 		Expect(json.message).toBe('Authorized successfully');
+// 	});
+// });
