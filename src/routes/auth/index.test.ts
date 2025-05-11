300ad7b03acbbca71e3595f2a0c08af2be61cf5f
Brandon Cha <bcha92@hotmail.com>
2025-05-10 20:53:29 -0400

    init vitest script

--- src/routes/auth/index.test.ts
+++ src/routes/auth/index.test.ts
@@ -6,17 +6,17 @@ import { MockEnvBindings } from '../../utils/testing.ts';
 const MOCK_ENV = new MockEnvBindings();
 
 describe('Sign-in route', () => {
-	test('Success', async () => {
+	test('Incorrect username/password should show unsuccessful login message', async () => {
 		const response = await app.request('/auth/sign-in', {
 			method: 'POST',
 			headers: new Headers({ 'Content-Type': 'application/json' }),
 			body: JSON.stringify({
-				email: 'test@example.com',
-				password: 'password123'
+				email: 'nope@example.com',
+				password: 'thisDoesNotWork'
 			})
 		}, MOCK_ENV);
 		const json: StatusResponse = await response.json();
 
-		expect(json.message).toBe('Authorized successfully');
+		expect(json.message).toBe('Either your email/password combination is invalid, or your account is not active');
 	});
 });
