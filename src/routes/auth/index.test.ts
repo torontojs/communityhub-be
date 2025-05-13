5cb884b7f1fd353c8a5d0ba6e76835dc7def6aba
Brandon Cha <bcha92@hotmail.com>
2025-05-12 21:06:26 -0400

    initial commit for sign up and sign in unit tests
    Co-authored-by: Lyla (Minju) Park <lylaminju@gmail.com>

--- src/routes/auth/index.test.ts
+++ src/routes/auth/index.test.ts
@@ -5,16 +5,81 @@ import { MockEnvBindings } from '../../utils/testing.ts';
 
 const MOCK_ENV = new MockEnvBindings();
 
+describe('Sign-up route', () => {
+	const signUpResponse = async (email: string, password: string, name: string) => {
+		const response = await app.request('/auth/sign-up', {
+			method: 'POST',
+			headers: new Headers({ 'Content-Type': 'application/json' }),
+			body: JSON.stringify({
+				email,
+				password,
+				name
+			})
+		}, MOCK_ENV);
+
+		const json: StatusResponse = await response.json();
+		console.log('SIGN-UPPITY', json);
+		return json;
+	};
+
+	test('Failure: registration should be valid email', async () => {
+		const json: StatusResponse = await signUpResponse('vitest', 'hashed_password_1', 'Vitest');
+		expect(json.message).toBe('');
+	});
+
+	test.todo('Failure: registration should fail if email is already registered');
+	test.todo('Failure: registration should have valid password');
+	test.todo('Failure: registration should have name');
+
+	test('Registration successful', async () => {
+		const json: StatusResponse = await signUpResponse('vitest@example.com', 'hashed_password_1', 'Vitest');
+
+		expect(json.message).toBe('Created a new profile and sent an email for confirmation');
+	});
+
+	test.todo('failure: activation should not be successful if incorrect token');
+
+	test.todo('failure: activation should not be successful if token expired');
+
+	test('Activation successful', async () => {
+		const response = await app.request('/auth/activate?token=abcd', {
+			method: 'GET'
+		}, MOCK_ENV);
+
+		const json: StatusResponse = await response.json();
+
+		expect(json.message).toBe('Account activated successfully');
+	});
+});
+
 describe('Sign-in route', () => {
-	test('Success', async () => {
+	test('Failure: Should not log in with incorrect username and/or password', async () => {
 		const response = await app.request('/auth/sign-in', {
 			method: 'POST',
 			headers: new Headers({ 'Content-Type': 'application/json' }),
 			body: JSON.stringify({
-				email: 'test@example.com',
-				password: 'password123'
+				email: 'notfound@example.com',
+				password: 'wrong'
 			})
 		}, MOCK_ENV);
+
+		const json: StatusResponse = await response.json();
+
+		expect(response.status).equals(401);
+		expect(json.message).toBe('Either your email/password combination is invalid, or your account is not active');
+	});
+	test.todo('Should not log in if account is not activated');
+
+	test('Success: Should log in successfully with correct username and password', async () => {
+		const response = await app.request('/auth/sign-in', {
+			method: 'POST',
+			headers: new Headers({ 'Content-Type': 'application/json' }),
+			body: JSON.stringify({
+				email: 'profile1@example.com',
+				password: 'hashed_password_1'
+			})
+		}, MOCK_ENV);
+		console.log('SIGN-INNIT', response);
 		const json: StatusResponse = await response.json();
 
 		expect(json.message).toBe('Authorized successfully');
